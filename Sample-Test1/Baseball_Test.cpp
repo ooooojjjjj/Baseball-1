776e2a7667f806d6d19682b85fc06bd32dd6e194
oj <ozoppp@gmail.com>
2023-06-02 09:21:19 +0900

    Check Invalid Argument

--- Sample-Test1/Baseball_Test.cpp
+++ Sample-Test1/Baseball_Test.cpp
@@ -1,6 +1,13 @@
 #include "pch.h"
 #include "../TDD_Baseball/Baseball.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
+using namespace std;
+
+TEST(TestCaseName, Exception_InvalidArgument) {
+    Baseball cBaseball;
+    
+    EXPECT_THROW(cBaseball.Guess("") , invalid_argument);
+    EXPECT_THROW(cBaseball.Guess("1234"), invalid_argument);
+    EXPECT_THROW(cBaseball.Guess("12V"), invalid_argument);
+    EXPECT_THROW(cBaseball.Guess("121"), invalid_argument);
 }
--- TDD_Baseball/Baseball.cpp
+++ TDD_Baseball/Baseball.cpp
@@ -1,4 +1,53 @@
+#include <string>
+#include <stdexcept>
+
+using namespace std;
 class Baseball
 {
 public:
+    bool IsNumbers(string& sInput)
+    {
+        for (char cEach : sInput)
+        {
+            if (cEach >= '0' && cEach <= '9') continue;
+            return true;
+        }
+
+        return false;
+    }
+
+    bool IsDuplicated(std::string& sInput)
+    {
+        return sInput[0] == sInput[1]
+            || sInput[0] == sInput[2]
+            || sInput[1] == sInput[2];
+    }
+
+    void CheckInputValidity(std::string& sInput)
+    {
+        if ("" == sInput)
+        {
+            throw invalid_argument("값을 넣어야 합니다.");
+        }
+
+        if (3 != sInput.length())
+        {
+            throw invalid_argument("자리 수 오류.");
+        }
+
+        if (IsNumbers(sInput))
+        {
+            throw invalid_argument("숫자로만 구성해야 합니다.");
+        }
+
+        if (IsDuplicated(sInput))
+        {
+            throw invalid_argument("숫자가 중복되면 안됩니다.");
+        }
+    }
+
+    void Guess(string sInput)
+    {
+        CheckInputValidity(sInput);
+    }
 };
